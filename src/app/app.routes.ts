1909a521f30130374d7a9b733bd26a6a35087dad
angelicareynaga <alma.gomez@cjj.gob.mx>
2025-04-24 14:48:53 -0600

    24 de Abril

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { DefaultComponent } from './layout/default/default.component';
+import { DashboardComponent } from './layout/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: DefaultComponent,
+        children: [
+         //   { path: 'login', component: LoginComponent}
+        ]
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent,
+        children: [
+         //   { path: 'login', component: LoginComponent}
+        ]
+    },
+];
